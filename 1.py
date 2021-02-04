6fa01473d19598b647aa4860f3de1afb47dfb26b
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-02-04 10:05:41 +0700

    Added 2 new files

---
+++ 1.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+names = ['Scott', 'Fred', 'Ann', 'Dave', 'Clara', 'Sam', 'Alex', 'Greg', 'Christina', 'Mary', 'Jane', 'Layla', 'Lucy']
